40f310695712c84ce1f4d06c9607d8b12bec173f
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-26 12:56:43 -0300

    :sparkles: Adding apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), './src/schema.gql'),
+      // sortSchema: true,
+      // debug: false,
+      // playground: false,
+    })
+  ]
 })
 export class AppModule {}
